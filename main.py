ee1d38792aa8274e9c451aa5c324ce1428f3d280
anisiabarros <anisia.barros@gmail.com>
2023-07-09 17:55:50 -0300

    usando a função find para encontrar a posiçao dos dados da url

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-url = "bytebank.com/cambio?moedaOrigem=real"
-print(url)
+url = "bytebank.com/cambio?moedaDestino=dolarmoedaOrigem=real"
 
 indice_interrogacao = url.find('?')
-url_base = url[0:indice_interrogacao]
-print(url_base)
+url_base = url[:indice_interrogacao]
 
-url_parametros = url[indice_interrogacao+1:36]
+url_parametros = url[indice_interrogacao+1:]
 print(url_parametros)
+
+parametro_busca = 'moedaOrigem'
+indice_parametro = url_parametros.find(parametro_busca)
+
+print(indice_parametro)
+
+indice_valor = indice_parametro + len(parametro_busca) + 1
+valor = url_parametros[indice_valor:]
+
+print(valor)
